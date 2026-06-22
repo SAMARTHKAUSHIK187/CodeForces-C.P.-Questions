791e915e41c3740abb133eb6990562d9a5de5e80
SAMARTHKAUSHIK187 <samarthkaushik187@gmail.com>
2026-06-22 22:55:37 +0530

    re-structure and git ignore

---
+++ C_Grandma_Capa_Knits_a_Scarf.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD (ll)(1e9+7)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while(t--) {
+        
+    }
+
+    return 0;
+}
